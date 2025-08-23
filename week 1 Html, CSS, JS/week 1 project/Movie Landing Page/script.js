52ecee16b5cf1b820b7985ef1ec8283f7e469c8d
Gaurav Kumar <gauravkumar.oistec@gmail.com>
2025-08-23 17:32:17 +0530

    second commit

---
+++ week 1 Html, CSS, JS/week 1 project/Movie Landing Page/script.js
@@ -0,0 +1,8 @@
+function changeBg(bg, title){
+    const banner = document.querySelector('.banner');
+    const contents = document.querySelectorAll('.content');
+    banner.style.content = ('.content');
+    banner.style.background = `url("images/${bg}")`;
+    banner.style.backgroundSize = 'cover';
+    banner.style.backgroundPosition = 'center';
+}
